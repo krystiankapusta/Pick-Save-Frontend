3d3daf13b7666496378bca2206f34e897902b1c5
krystiankapusta <krystian.kapusta@student.up.krakow.pl>
2025-06-21 20:14:25 +0200

    Add footer with styling

---
+++ src/Components/Footer/Footer.tsx
@@ -0,0 +1,10 @@
+import './Footer.css';
+const Footer = () => {
+    return (
+        <footer>
+            <p>&copy; 2025 Pick&Save Website</p>
+        </footer>
+    );
+};
+
+export default Footer;
